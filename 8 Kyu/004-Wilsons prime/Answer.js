88f78d704f7034844ac73214743b41598250074d
Ehsan Langroudi (kendrick) <ehsanlangroud.kk@gmail.com>
2022-08-11 11:16:56 +0430

    yup im mad :D 004- wilson primes added

---
+++ 8 Kyu/004-Wilsons prime/Answer.js
@@ -0,0 +1,7 @@
+// the answer is kinda bull shit BUT it tought me to fully understand the informations anad problems given or i will spen an hour tryin to fix a problem thats for super computers (^o^')>
+
+// this is a Great answer by "ooflorent"
+
+function amIWilson(p) {
+  return p === 5 || p === 13 || p === 563;
+}
